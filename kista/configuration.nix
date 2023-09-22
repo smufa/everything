3ea196092d134c94d7c7797ff28a1c7a089e252e
enei <enei.sluga@gmail.com>
2023-09-22 15:25:45 +0200

    neki malega

--- kista/configuration.nix
+++ kista/configuration.nix
@@ -13,6 +13,7 @@
     ../lib/space-optimization.nix
     ../lib/locale.nix
   ];
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -24,9 +25,12 @@
   environment.systemPackages = with pkgs; [
     beep
   ];
+  # boot.initrd.kernelModules = [ "nvidia" ];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
   # Extend life of SSD
   services.fstrim.enable = true;
+  services.fwupd.enable = true;
 
   networking.hostName = "kista"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -127,19 +131,21 @@
   users.users = {
     enei = {
       isNormalUser = true;
-      description = "enei";
+      description = "Enei";
       extraGroups = ["networkmanager" "wheel" "uinput" "input"];
       shell = pkgs.fish;
       packages = with pkgs; [
-        firefox
+        firefox-wayland
         ungoogled-chromium
+        epiphany
         #  thunderbird
       ];
     };
     tina = {
       isNormalUser = true;
-      description = "enei";
+      description = "Tina";
       extraGroups = ["networkmanager" "wheel"];
+      shell = pkgs.fish;
       packages = with pkgs; [
         firefox
         #  thunderbird
@@ -151,11 +157,33 @@
   nixpkgs.config.allowUnfree = true;
 
   # Nvidia
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver (also valid for Wayland)
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  hardware.nvidia.powerManagement.enable = true;
+
+  hardware.nvidia = {
+
+    # Modesetting is needed for most Wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    powerManagement.enable = true;    
+  };
   services.xserver.displayManager.gdm.autoSuspend = false;
 
   # Enable flatpak
